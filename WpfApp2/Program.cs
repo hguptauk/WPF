6768531083400d9885d85efabad12d548ce3ef4c
hguptauk <HGUPTAUK@GMAIL.COM>
2024-02-22 12:30:08 +0000

    Blank WPF App using .net core

---
+++ WpfApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp2
+{
+    public class Program : Application
+    {
+        [STAThread()]
+        static void Main()
+        {
+            // Create a program that will show two different windows,
+            // and will wait until both are closed before ending.
+            Program app = new Program();
+            app.ShutdownMode = ShutdownMode.OnLastWindowClose;
+
+            //// First approach: window with XAML content.
+            //Window1 window1 = new Window1("Window1.xaml");
+            //window1.Show();
+
+            //// Second approach: XAML for complete window.            
+            //Xaml2009Window window2 = Xaml2009Window.LoadWindowFromXaml("Xaml2009.xaml");
+            //window2.Show();
+            Window window1 = new MainWindow();
+            window1.Show();
+            Window window2 = new MainWindow();
+            window2.Show();
+            app.Run();
+        }
+    }
+}
