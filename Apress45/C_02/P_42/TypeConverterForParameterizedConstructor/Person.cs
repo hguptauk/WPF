fd7d133e425b85d0dc5858f1c551f54661f3c52d
hguptauk <HGUPTAUK@GMAIL.COM>
2024-02-26 21:34:06 +0000

    Added new project for Calling a parameterized constructor from XAML using TypeConverter at class level

---
+++ Apress45/C_02/P_42/TypeConverterForParameterizedConstructor/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConverterForParameterizedConstructor
+{
+    [TypeConverter(typeof(PersonTypeConverter))]
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string WelcomeMessage => $"Hi I am {Name} and I am {Age} yr old";
+    }
+}
---
+++ Apress45/C_02/P_42/TypeConverterForParameterizedConstructor/PersonTypeConverter.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+
+namespace TypeConverterForParameterizedConstructor
+{
+    public class PersonTypeConverter : TypeConverter
+    {
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+
+
+            // We can convert from Person to string
+            if (destinationType == typeof(string))
+            {
+                return true;
+            }
+
+            return base.CanConvertTo(context, destinationType);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+        {
+            if (value is Person person && destinationType == typeof(string))
+            {
+                return $"{person.Name}, is {person.Age} years old";
+            }
+
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+        {
+            // Convert the string to a Person object
+            if (value is string stringValue)
+            {
+                string[] parts = stringValue.Split(',');
+                if (parts.Length == 2)
+                {
+                    string name = parts[0].Trim();
+                    if (int.TryParse(parts[1].Trim(), out int age))
+                    {
+                        return new Person(name, age);
+                    }
+                }
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            // We can convert from string to Person
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+
+            return base.CanConvertFrom(context, sourceType);
+        }
+    }
+}
